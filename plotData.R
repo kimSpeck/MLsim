84eb8cd631f78eb82d69fb6a361aefe79aed01a6
kimSpeck <kimlaura.s@web.de>
2023-10-10 13:07:16 +0200

    plot bias in estimated coefficients and train/test performance measures

--- plotData.R
+++ plotData.R
@@ -1,69 +1,51 @@
 library(ggplot2)
 
 source("setParameters.R")
+source("analysisTools.R")
 
 plotFolder <- "plots"
 if (!file.exists(plotFolder)){
   dir.create(plotFolder)
 }
 
-resFolder <- "results"
-load(paste0(resFolder, "/estB_initialResults.rda"))
-
-condGrid <- expand.grid(N = setParam$dgp$N, 
+condGrid <- expand.grid(N = setParam$dgp$N,
                         pTrash = setParam$dgp$pTrash)
+condN_pTrash <- paste0("N", condGrid$N, "_pTrash", condGrid$pTrash)
 
-condLabels <- sapply(seq_len(nrow(condGrid)), function(x) {
-  paste0("N", condGrid[x, "N"], "_pTrash", condGrid[x, "pTrash"])})
-names(res_estB) <- condLabels
+resFolder <- "results/resultsServer"
+load(paste0(resFolder, "/fullData.rda"))
 
-linEffects <- sapply(seq_len(setParam$dgp$p), function(x) paste0("Var", x))
-# choose variables for interaction that have no linear effects (R2 budget)
-# interEffects <- c("Var1:Var2", "Var1:Var4", "Var2:Var3", "Var3:Var4")
-interEffects <- c("Var5:Var6", "Var5:Var8", "Var6:Var7", "Var7:Var8")
-nEffects <- length(c(linEffects, interEffects))
+################################################################################
+# plot relative bias in estimated coefficients 
+################################################################################
+# pull data from nested list of all results (fullData)
+estBeta <- rbindResults(fullData, "estBeta")
+estBeta$Var <- rownames(estBeta)
 
+# variables from rownames to own column to work woth variable information
+estBeta$Var <- stringr::str_replace(estBeta$Var, "\\.[:digit:]{1,}$", "")
+estBeta$Var <- stringr::str_replace(estBeta$Var, "\\.", ":")
 
-tmp_effects <- lapply(res_estB, function(x) {
-  idxM <- which(rownames(x[["estB_M"]]) %in% c(linEffects, interEffects))
-  idxSD <- which(rownames(x[["estB_SD"]]) %in% c(linEffects, interEffects))
-  idxSE <- which(rownames(x[["estB_SE"]]) %in% c(linEffects, interEffects))
-  subList <- list(estB_M = x[["estB_M"]][idxM,],
-                  estB_SD = x[["estB_SD"]][idxSD,],
-                  estB_SE = x[["estB_SE"]][idxSE,])
-  subList <- do.call(cbind, subList)
-  addVarName <- rep(c("estB_M", "estB_SD", "estB_SE"), each = length(setParam$dgp$condLabels))
-  colnames(subList) <- paste0(addVarName, "__", colnames(subList))
-  subList
-})
-names(tmp_effects) <- condLabels
-estB_effects <- do.call(rbind, tmp_effects)
-estB_effects <- cbind(N_pTrash = rep(names(tmp_effects), each = nEffects), 
-                      data.frame(estB_effects))
-estB_effects$effect <- rep(c(linEffects, interEffects), times = length(condLabels))
+# only keep variables with simulated effects 
+idxEstBeta <- which(estBeta$Var %in% c(setParam$dgp$linEffects, setParam$dgp$interEffects))
+# # check if idx really catches all effects
+# length(idxEstBeta) == length(setParam$dgp$condLabels) *
+#   length(condN_pTrash) * (length(setParam$dgp$linEffects) + length(setParam$dgp$interEffects))
 
-estB_effects <- tidyr::pivot_longer(estB_effects, cols = !c(N_pTrash, effect), names_to = "type", values_to = "values")
-estB_effects <- tidyr::separate(estB_effects, N_pTrash, c("N", "pTrash"), sep = "_")
-estB_effects <- tidyr::separate(estB_effects, type, c("measure", "type"), sep = "__")
+estB_effects <- estBeta[idxEstBeta,]
 
-estB_effects$N <- stringr::str_sub(estB_effects$N, start = 2L)
-estB_effects$pTrash <- stringr::str_sub(estB_effects$pTrash, start = 7L)
+# get informative variables for simulated conditions (N, pTrash, R2, lin_inter)
+estB_effects <- idx2info(estB_effects)
 
-estB_effects$R2 <- stringr::str_sub(estB_effects$type, start = 3L, end = 5L)
-estB_effects$lin_inter <- stringr::str_sub(estB_effects$type, start = -7L, end = -1L)
-# estB_effects <- tidyr::separate(estB_effects, lin_inter, c("lin", "inter"), sep = "_")
-
-estB_effects$type <- NULL
-
-estB_effects <- tidyr::pivot_wider(estB_effects, names_from = measure, values_from = values)
-
+# change type of columns or specific entry details to prepare plotting  
 str(estB_effects)
 estB_effects$N <- factor(estB_effects$N, levels = setParam$dgp$N)
 estB_effects$pTrash <- factor(estB_effects$pTrash, levels = sort(setParam$dgp$pTrash, decreasing = T))
 
-estB_effects$effectType <- ifelse(estB_effects$effect %in% 
-                                    unique(estB_effects$effect)[stringr::str_detect(unique(estB_effects$effect), ":")], "inter", "lin")
+estB_effects$effectType <- ifelse(estB_effects$Var %in% 
+                                    unique(estB_effects$Var)[stringr::str_detect(unique(estB_effects$Var), ":")], "inter", "lin")
 
+# add true simulated effects to the data frame to be able to calculate biases in estimated coefficients
 trueB <- data.frame(R2 = setParam$dgp$Rsquared, setParam$dgp$trueEffects)
 trueB <- tidyr::pivot_longer(trueB, cols = !R2, names_to = "lin", values_to = "trueB_lin")
 trueB$lin <- stringr::str_sub(trueB$lin, start = 2L)
@@ -82,10 +64,11 @@ trueB <- tidyr::pivot_longer(trueB, cols = c(lin, inter), names_to = "effectType
 trueB$R2 <- as.character(trueB$R2) # identical variable types to merge correctly
 estB_effects <- merge(estB_effects, trueB, by = c("R2", "lin_inter", "effectType"))
 
+# calculate absolute & relative bias
 # absolute bias biases interpretation of deviation in estimated coefficients due to different sizes of 
 #     simulated coefficients depending on R2 budget size
-estB_effects$absBiasB <- estB_effects$estB_M - estB_effects$trueB
-estB_effects$relBiasB <- (estB_effects$estB_M - estB_effects$trueB) /  estB_effects$trueB
+estB_effects$absBiasB <- estB_effects$M - estB_effects$trueB
+estB_effects$relBiasB <- (estB_effects$M - estB_effects$trueB) /  estB_effects$trueB
 
 
 linInterTypes <- unique(estB_effects$lin_inter)
@@ -97,20 +80,21 @@ estB_effects$lin_inter <- factor(estB_effects$lin_inter,
                                  labels = linIntLabels)
 
 # to do:
-#   - plot for all variables
+#   - pTrash/
 
+# plot relative bias for every variable with simulated effects separately 
 colValues <- c("green3", "darkcyan", "darkblue", "darkmagenta")
 
-plotRelBias <- function(data) {
+plotRelBias <- function(data) { # plot function
   ggplot(data,
          aes(x = interaction(pTrash, N, sep = " x "), y = relBiasB, group = R2, colour = R2)) +
     geom_point() +
     geom_line() +
-    geom_errorbar(aes(ymin = relBiasB - estB_SE, ymax = relBiasB + estB_SE), width=.2) +
+    geom_errorbar(aes(ymin = relBiasB - SE, ymax = relBiasB + SE), width=.2) +
     scale_y_continuous(limits = c(-1, 0), breaks = seq(-1, 0, 0.2)) +
     scale_color_manual(values = colValues) +
     geom_hline(aes(yintercept = 0)) +
-    facet_wrap(~lin_inter) +
+    facet_wrap(~ lin_inter) +
     ylab("relative bias in est. \u03B2") +
     xlab("pTrash (decreasing) x N (increasing)") +
     ggtitle(paste0("relativ bias for ", iVar)) +
@@ -119,18 +103,20 @@ plotRelBias <- function(data) {
           axis.title.x = element_text(size = 20),
           axis.title.y = element_text(size = 20))
 }
-  
-for (iVar in c(linEffects, interEffects)) {
-  subset <- estB_effects[which(estB_effects$effect == iVar), ]
+
+# generate plot for every variable with simulated effects
+for (iVar in c(setParam$dgp$linEffects, setParam$dgp$interEffects)) {
+  subset <- estB_effects[which(estB_effects$Var == iVar), ]
   tmp_p <- plotRelBias(subset)
   
   plotName <- paste0("pRelBias_", sub(':','.', iVar))
   assign(plotName, tmp_p)
 }
 
+# save all plots as files
 plotNames <- ls(pattern = "^pRelBias")
 for (iPlot in plotNames) {
-  pName <- paste0(iPlot, "_initialResults")
+  pName <- paste0(iPlot, "_testResults")
     
   ggplot2::ggsave(filename = paste0(plotFolder, "/", pName, ".eps"),
                     plot = eval(parse(text = iPlot)),
@@ -147,25 +133,68 @@ for (iPlot in plotNames) {
                     units = "in") 
 }
 
-# # test plots
-# # absolute bias
-# ggplot(estB_effects[which(estB_effects$effect == "Var1"), ],
-#        aes(x = interaction(pTrash, N), y = absBiasB, group = R2, colour = R2)) +
-#   geom_point() +
-#   geom_line() +
-#   scale_color_manual(values = colValues) +
-#   geom_hline(aes(yintercept = 0)) +
-#   geom_errorbar(aes(ymin = absBiasB - estB_SE, ymax = absBiasB + estB_SE), width=.2) +
-#   facet_wrap(~lin_inter) +
-#   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
+################################################################################
+# plot train and test performance
+################################################################################
+# pull data from nested list of all results (fullData)
+performanceTrain <- rbindResults(fullData, "performTrainStats")
+performanceTest <- rbindResults(fullData, "performTestStats")
 
-# # averaged coeffients
-# ggplot(estB_effects[which(estB_effects$effect == "Var1"), ],
-#        aes(x = interaction(pTrash, N), y = estB_M, group = R2, colour = R2)) +
-#   geom_point() +
-#   geom_line() +
-#   scale_color_manual(values = colValues) +
-#   geom_errorbar(aes(ymin = estB_M - estB_SE, ymax = estB_M + estB_SE), width=.2) +
-#   facet_wrap(~lin_inter) +
-#   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
+# variables from rownames to own column to work woth variable information
+performanceTrain$measure <- rownames(performanceTrain)
+performanceTrain$measure <- stringr::str_replace(performanceTrain$measure, "\\.[:digit:]{1,}$", "")
+performanceTrain$measure <- paste0(performanceTrain$measure, "_train")
 
+performanceTest$measure <- rownames(performanceTest)
+performanceTest$measure <- stringr::str_replace(performanceTest$measure, "\\.[:digit:]{1,}$", "")
+performanceTest$measure[which(performanceTest$measure == "Rsq_test")] <- "Rsquared_test"
+
+# get informative variables for simulated conditions (N, pTrash, R2, lin_inter) 
+performanceTrain <- idx2info(performanceTrain)
+performanceTest <- idx2info(performanceTest)
+
+# change type of columns or specific entry details to prepare plotting  
+performanceTrain$N <- factor(performanceTrain$N, levels = setParam$dgp$N)
+performanceTrain$pTrash <- factor(performanceTrain$pTrash, levels = sort(setParam$dgp$pTrash, decreasing = T))
+
+performanceTest$N <- factor(performanceTest$N, levels = setParam$dgp$N)
+performanceTest$pTrash <- factor(performanceTest$pTrash, levels = sort(setParam$dgp$pTrash, decreasing = T))
+
+# merge performance Train and performance Test
+performanceStats <- rbind(performanceTrain, performanceTest)
+performanceStats <- tidyr::separate(performanceStats, measure, c("measure", "trainTest"), sep = "_")
+
+# plot performance measures for train and test data
+colValues <- c("green3", "darkcyan", "darkblue", "darkmagenta")
+
+pPerformTrainVStest <- ggplot(performanceStats[which(performanceStats$measure != "MAE"),],
+       aes(x = interaction(pTrash, N, sep = " x "), y = M, 
+           group = R2, colour = R2)) +
+  geom_point() +
+  geom_line() +
+  geom_errorbar(aes(ymin = M - SE, ymax = M + SE), width=.2) +
+  scale_color_manual(values = colValues) +
+  geom_hline(aes(yintercept = 0)) +
+  facet_grid(measure + trainTest ~ lin_inter, scales = "free_y", labeller = label_both, switch = "y") +
+  ylab("") +
+  xlab("pTrash (decreasing) x N (increasing)") +
+  ggtitle("Training vs. Test performance") +
+  theme(axis.text.y = element_text(size = 20),
+        axis.text.x = element_text(size = 15, angle = 45, vjust = 1, hjust=1),
+        axis.title.x = element_text(size = 20),
+        axis.title.y = element_text(size = 20))
+
+# save plots as files
+ggplot2::ggsave(filename = paste0(plotFolder, "/performanceTrainTest.eps"),
+                plot = pPerformTrainVStest,
+                device = cairo_ps,
+                dpi = 300,
+                width = 13.95,
+                height = 11.51,
+                units = "in")
+
+ggplot2::ggsave(filename = paste0(plotFolder, "/performanceTrainTest.png"),
+                plot = pPerformTrainVStest,
+                width = 13.95,
+                height = 11.51,
+                units = "in") 
