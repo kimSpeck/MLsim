8e8960ac61eb08e1d83df8d8c99eb8bacd8150a4
kimSpeck <kimlaura.s@web.de>
2023-10-24 19:19:51 +0200

    one PSM correlation matrix for all samples

--- setParameters.R
+++ setParameters.R
@@ -24,7 +24,10 @@ setParam$dgp$nModelPredictors <- P * setParam$dgp$poly + choose(P, setParam$dgp$
 # P * setParam$dgp$poly + (P * (P-1) / 2) # this only works for interactionDepth = 2
 rm(P)
 
-# 
+################################################################################
+# simulated effects
+################################################################################
+# linear effects
 setParam$dgp$linEffects <- sapply(seq_len(setParam$dgp$p), function(x) paste0("Var", x))
 # choose variables for interaction that have no linear effects (R2 budget)
 # interEffects <- c("Var1:Var2", "Var1:Var4", "Var2:Var3", "Var3:Var4")
@@ -40,7 +43,9 @@ if (!all(setParam$dgp$percentLinear+
          setParam$dgp$percentInter +
          setParam$dgp$percentPoly == 1)) stop("Proportion of different kinds of effects do not sum up to 1! Check values!")
 
-# r squared
+################################################################################
+# R squared
+################################################################################
 setParam$dgp$Rsquared <- c(.10, .30, .50, .80)
 
 setParam$dgp$trueEffects <- cbind(p0.5 = c(0.116, 0.227, 0.346, 0.693),
@@ -53,18 +58,60 @@ comboGrid <- expand.grid(setParam$dgp$Rsquared,
 setParam$dgp$condLabels <- sapply(seq_len(length(setParam$dgp$Rsquared) * length(setParam$dgp$percentLinear)), 
                                   function(x) paste0("R2", comboGrid$Var1[x], "lin_inter", comboGrid$Var2[x]))
 rm(comboGrid)
-# average correlations of predictors and their SD 
+
+################################################################################
+# predictor correlations
 # ToDo: manipulate correlations between different variables (no separate simulated conditions)
+################################################################################
+# do not randomly sample correlation matrix in each sample!
+# instead: randomly choose one correlation matrix and use the same correlation matrix in each sample
+setParam$dgp$Reffects <- 0.4 # correlation between predictors with simulated effects
+
+# average correlations of trash-predictors and their SD 
 setParam$dgp$meanR <- 0
-setParam$dgp$sdR <- 0.1
+setParam$dgp$sdR <- 0.05 # 0.1 leads to non-PSM correlation matrix
 # setParam$dgp$sdR <- 0.2 # original value
+
+# use this correlation matrix in data simulation! 
+# set seed
+set.seed(42)
+
+# randomly choose correlation matrix for biggest set of predictors (max(pTrash))
+# (use subsets for pTrash < max(pTrash))
+P <- max(setParam$dgp$p + setParam$dgp$pTrash)
+corN <- P*(P-1)/2
+corNp <- setParam$dgp$p*(setParam$dgp$p-1)/2
+
+repeat{
+  # correlations around 0 for trash variables (# alternatively exactly 0 correlations)
+  corVec <- rtruncnorm(corN, mean = setParam$dgp$meanR, sd = setParam$dgp$sdR, 
+                       a = -0.5, b = 0.5) 
+  rX <- lavaan::lav_matrix_upper2full(corVec, diagonal = F) # fill up upper triangle
+  # rX <- lavaan::lav_matrix_upper2full(rep(0, corN), diagonal = F) # fill up upper triangle
+  
+  # correlations between predictor variables (all identical atm)
+  rP <- lavaan::lav_matrix_upper2full(rep(setParam$dgp$Reffects, corNp), diagonal = F)
+  rX[1:dim(rP)[1], 1:dim(rP)[1]] <- rP
+  diag(rX) <- 1
+  
+  # check if correlation matrix is positive semi-definite
+  # thereby avoid warning and correction procedure in rmvnorm
+  if(all(eigen(rX)$values >= 0)) {
+    break
+  }
+}
+setParam$dgp$predictorCorMat <- rX # save PSM correlation matrix
+rm(P, corN, corNp, corVec, rP, rX) # remove temporary variables
 
 # error "variance" (= standard deviation)
 setParam$dgp$sigmaE <- 1
 
+# measurement error in predictors
+setParam$dgp$reliability <- c(0.5, 0.7, 1)
+
 # parameter for gbm or model fitting more general
 setParam$fit$lambda <- 10^seq(-1, 1, length = 100)
-setParam$fit$alpha <- seq(0, 1, .1) 
+setParam$fit$alpha <- seq(0, 1, length.out = 20) # alpha with at least 20 steps
 setParam$fit$nfolds <- 10
 setParam$fit$lambdaCrit <- "1se" # min or 1se
 if (!(setParam$fit$lambdaCrit %in% c("min", "1se"))) {
