ba27967eb94078916f25f291392241e657c89fee
kimSpeck <kimlaura.s@web.de>
2023-11-28 16:21:03 +0100

    add reliability as factor in simulation design

--- analysisTools.R
+++ analysisTools.R
@@ -28,9 +28,10 @@ idx2info <- function(data) {
   data$condLabel <- setParam$dgp$condLabel[data$idxCondLabel]
   data$N_pTrash <- condN_pTrash[data$idxN_pTrash]
   
-  data <- tidyr::separate(data, N_pTrash, c("N", "pTrash"), sep = "_")
+  data <- tidyr::separate(data, N_pTrash, c("N", "pTrash", "rel"), sep = "_")
   data$N <- stringr::str_sub(data$N, start = 2L)
   data$pTrash <- stringr::str_sub(data$pTrash, start = 7L)
+  data$rel <- stringr::str_sub(data$rel, start = 4L)
   
   data$R2 <- stringr::str_sub(data$condLabel, start = 3L, end = 5L)
   data$lin_inter <- stringr::str_sub(data$condLabel, start = -7L, end = -1L)
