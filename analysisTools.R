aba2f309a4fdef36c4d06cbf3f2c19dab94fae88
kimSpeck <kimlaura.s@web.de>
2024-03-13 21:45:21 +0100

    substitute index with human readable data information

--- analysisTools.R
+++ analysisTools.R
@@ -17,7 +17,26 @@ rbindResults <- function(data, result) {
   return(data.frame(tmpRes))
 }
 
-idx2info <- function(data) {
+
+rownames2col <- function(data, varName = "rownames") {
+  ###
+  # variables from rownames to own column to work woth variable information
+  ## input:
+  # data    - [data frame] specific result in one data frame 
+  # varName - [character string] name for new column with rowname information
+  ## output:
+  # data    - [data.frame] new column with rownames information
+  ###
+  # rownames to column in data frame
+  data$tmp <- rownames(data)
+  # remove numbering (rownames need to be unique values, thus suffices .1, .2, ...)
+  data$tmp <- stringr::str_replace(data$tmp, "\\.[:digit:]{1,}$", "")
+  # name new column
+  names(data)[names(data) == 'tmp'] <- varName
+  return(data)
+}
+
+idx2info <- function(data, cond_Np, type) {
   ###
   # replace idx values with informative labels (used to be matrices therefore idx **numbers**)
   ## input:
@@ -25,15 +44,24 @@ idx2info <- function(data) {
   ## output:
   # data    - [data.frame] numeric idx columns replaced by character type label columns
   ###
-  data$condLabel <- setParam$dgp$condLabel[data$idxCondLabel]
-  data$N_pTrash <- condN_pTrash[data$idxN_pTrash]
+  data$idxCondLabel <- as.numeric(data$idxCondLabel)
+  data$idxN_pTrash <- as.numeric(data$idxN_pTrash)
   
-  data <- tidyr::separate(data, N_pTrash, c("N", "pTrash", "rel", "factor", "indicators"), sep = "_")
+  data$condLabel <- setParam$dgp$condLabel[data$idxCondLabel]
+  data$N_pTrash <- cond_Np[data$idxN_pTrash]
+  
+  if (type == "enet") {
+    data <- tidyr::separate(data, N_pTrash, c("N", "pTrash", "rel", "factor", "indicators"), sep = "_")
+    data$factor <- stringr::str_sub(data$factor, start = 2L)
+    data$indicators <- stringr::str_sub(data$indicators, start = 4L)
+  } else if (type == "gbm") {
+    data <- tidyr::separate(data, N_pTrash, c("N", "pTrash", "rel"), sep = "_")  
+  }
+  
   data$N <- stringr::str_sub(data$N, start = 2L)
   data$pTrash <- stringr::str_sub(data$pTrash, start = 7L)
   data$rel <- stringr::str_sub(data$rel, start = 4L)
-  data$factor <- stringr::str_sub(data$factor, start = 2L)
-  data$indicators <- stringr::str_sub(data$indicators, start = 4L)
+  
   
   data$R2 <- stringr::str_sub(data$condLabel, start = 3L, end = 5L)
   data$lin_inter <- stringr::str_sub(data$condLabel, start = -7L, end = -1L)
