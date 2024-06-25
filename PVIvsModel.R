5e4b23e2589cd99cf6ba2521cc598b35bff6b0e2
kimSpeck <kimlaura.s@web.de>
2024-06-25 18:58:30 +0200

    initial commit

---
+++ PVIvsModel.R
@@ -0,0 +1,8 @@
+# compare PVI and ENET predictors
+# is PVI > 1 equivalent to predictor selection based on regularization?
+
+# can we somehow check this for GBMs as well?
+
+# 1. read in PVI data and ENET model data
+# 2. compare if variables are extracted or not?
+# confusion matrix: extracted in ENET {yes, no} vs. PVI > 1 {yes, no}
