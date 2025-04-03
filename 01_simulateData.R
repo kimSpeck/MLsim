b9d3c8eb0ee51d483ac0193d6b71ef72802482df
kimSpeck <kimlaura.s@web.de>
2025-04-03 13:46:40 +0200

    adapt architecture to model nonlinear data, too

--- 01_simulateData.R
+++ 01_simulateData.R
@@ -19,6 +19,11 @@ timeStampFolder <- format(Sys.time(), "%d%m%y_%H%M%S")
 dataFolder <- "data"
 createFolder(dataFolder)
 
+dgpFolder <- "/inter"
+createFolder(paste0(dataFolder, dgpFolder))
+dgpFolder <- "/nonlinear"
+createFolder(paste0(dataFolder, dgpFolder))
+
 # generate folder for log files
 logFolder = "log"
 createFolder(logFolder)
@@ -29,25 +34,41 @@ nCoresSampling <- 5 # simulate data
 # grid to simulate data with mapply later
 # simulation via mapply to easily simulate subsets of parameter combinations
 # create factor structure for predictor variables to increase reliability
-gridFull <- expand.grid(N = setParam$dgp$N,
+gridInter <- expand.grid(N = setParam$dgp$N,
                         pTrash = setParam$dgp$pTrash,
                         reliability = setParam$dgp$reliability)
 # ! factors are interpreted as level numbers!; only character variables are interpreted by their name!
-str(gridFull) 
+str(gridInter) 
 
 # create seed number for parallel cluster (reproducibility of generated data)
 set.seed(8967369)
-seedNum <- sample(1:999999, dim(gridFull)[1], replace = FALSE) 
-gridFull$sampleSeed <- seedNum[1:dim(gridFull)[1]]
+seedNum <- sample(1:999999, dim(gridInter)[1], replace = FALSE) 
+gridInter$sampleSeed <- seedNum[1:dim(gridInter)[1]]
+
+gridFull <- cbind(data = "inter", gridInter)
+
+set.seed(4890920)
+seedNum <- sample(1:999999, dim(gridInter)[1], replace = FALSE) 
+
+gridFull <- rbind(gridFull, 
+                  cbind(data = "nonlinear", 
+                        gridInter[,!colnames(gridInter) %in% "sampleSeed"], 
+                        sampleSeed = seedNum))
 
 # sample data in parallel
-createData <- function(N, pTrash, reliability, sampleSeed){
+createData <- function(data, N, pTrash, reliability, sampleSeed){
   
-  environment(sampleInteractionData) <- environment()
+  if (data == "inter"){
+    environment(sampleInteractionData) <- environment()  
+  } else if (data == "nonlinear") {
+    environment(sampleNonlinearData) <- environment()  
+  } else {
+    stop("We can only simulate inter or nonlinear data!")
+  }
   
   # Initiate cluster; type = "FORK" only on Linux/MacOS: contains all environment variables automatically
   cl <- makeCluster(nCoresSampling, type = "FORK",
-                    outfile = paste0(logFolder, "/", "simulateDataStatus",
+                    outfile = paste0(logFolder, "/", "simulateDataStatus_", data, 
                                      timeStampFolder, ".txt"))
   
   # set seed that works for parallel processing
@@ -55,14 +76,22 @@ createData <- function(N, pTrash, reliability, sampleSeed){
   s <- .Random.seed
   clusterSetRNGStream(cl = cl, iseed = s)
   
-  sampleInteractionData() # run function to actually create dataset
+  if (data == "inter"){
+    sampleInteractionData() # run function to actually create dataset
+  } else if (data == "nonlinear") {
+    sampleNonlinearData()
+  }
   
   # close cluster to return resources (memory) back to OS
   stopCluster(cl)
 }
 
-# # test it
+# # test simulating data for only one condition
 # out <- do.call(mapply, c(FUN = createData, gridFull[1,]))
 
-out <- do.call(mapply, c(FUN = createData, gridFull))
+# simulate only nonlinear data
+out <- do.call(mapply, c(FUN = createData, gridFull[gridFull$data == "nonlinear", ]))
+
+# # simulate full data
+# out <- do.call(mapply, c(FUN = createData, gridFull))
 
